7c2dc8028e4835411a6d00802c417265bf0d8439
arinjay <arinjaysharma421@gmail.com>
2017-02-15 20:09:06 +0530

    designing views

--- BucketList/MainVC.swift
+++ BucketList/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  BucketList
 //
 //  Created by Arinjay Sharma on 2/15/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
